69284a0120001a493cca1e5ecdc55c15a2bb0588
mesheth <mehul.sheth@oracle.com>
2020-05-22 20:16:35 +0530

    Update Completeness of Binary Tree with examples

--- DataStructures/src/com/sheth/test/trees/misc/TestValidateCompletenessOfABinaryTree.java
+++ DataStructures/src/com/sheth/test/trees/misc/TestValidateCompletenessOfABinaryTree.java
@@ -10,6 +10,30 @@ import com.sheth.trees.misc.ValidateCompletenessOfABinaryTree;
 
 class TestValidateCompletenessOfABinaryTree {
 
+	/* Input 1
+	 * 
+				10
+		    /         \  
+		  5           20  
+		 /  \         /  \
+		9    18     3     7
+
+	 *
+	 */
+
+	/* Input 2
+	 * 
+				10
+		    /         \  
+		  5           20  
+		 /  \         /  \
+		9    18     3     7
+	   / \    \
+	  11  17   17	
+	 *
+	 */
+
+	
 	private ValidateCompletenessOfABinaryTree<Integer> testValidateCompletenessOfABinaryTree;
 	private BinaryTreeNode<Integer> root;
 
@@ -23,9 +47,6 @@ class TestValidateCompletenessOfABinaryTree {
 		BinaryTreeNode<Integer> n4 = new BinaryTreeNode<Integer>(18);
 		BinaryTreeNode<Integer> n5 = new BinaryTreeNode<Integer>(3);
 		BinaryTreeNode<Integer> n6 = new BinaryTreeNode<Integer>(7);
-		BinaryTreeNode<Integer> n7 = new BinaryTreeNode<Integer>(11);
-		BinaryTreeNode<Integer> n8 = new BinaryTreeNode<Integer>(17);
-		BinaryTreeNode<Integer> n9 = new BinaryTreeNode<Integer>(17);
 		
 		root.setLeft(n1);
 		root.setRight(n2);
@@ -33,15 +54,22 @@ class TestValidateCompletenessOfABinaryTree {
 		n1.setRight(n4);
 		n2.setLeft(n5);
 		n2.setRight(n6);
-//		n3.setLeft(n7);
-//		n3.setRight(n8);
-//		n4.setRight(n9);
 	}
 
 	@Test
 	void testIsCompleteBinaryTree() {
 		boolean isCompleteBinaryTree = testValidateCompletenessOfABinaryTree.isCompleteBinaryTree(root);
 		assertEquals(true, isCompleteBinaryTree);
+		
+		BinaryTreeNode<Integer> n7 = new BinaryTreeNode<Integer>(11);
+		BinaryTreeNode<Integer> n8 = new BinaryTreeNode<Integer>(17);
+		BinaryTreeNode<Integer> n9 = new BinaryTreeNode<Integer>(17);
+		root.getLeft().getLeft().setLeft(n7);
+		root.getLeft().getLeft().setRight(n8);
+		root.getLeft().getRight().setRight(n9);
+		isCompleteBinaryTree = testValidateCompletenessOfABinaryTree.isCompleteBinaryTree(root);
+		assertEquals(false, isCompleteBinaryTree);
+
 	}
 
 }
--- DataStructures/src/com/sheth/trees/misc/ValidateCompletenessOfABinaryTree.java
+++ DataStructures/src/com/sheth/trees/misc/ValidateCompletenessOfABinaryTree.java
@@ -5,36 +5,65 @@ import java.util.Queue;
 
 import com.sheth.trees.common.BinaryTreeNode;
 
+/**
+ * Use queue (BFT) to iterate through nodes for Complete Binary Tree
+ * 
+ * 1. Mark flag to true if left or right is null for current node
+ * 
+ * 2. Keep adding left and right node of the current node to queue only if flag
+ * is not set to true
+ * 
+ * 3. If flag is set to true, it represents that previous node in the traversal
+ * was already null which disqualifies for Complete Binary Tree
+ * 
+ * @author mesheth
+ *
+ * @param <T>
+ */
 public class ValidateCompletenessOfABinaryTree<T> {
 
 	public boolean isCompleteBinaryTree(BinaryTreeNode<T> node) {
-		boolean flag = false;
+		boolean encounteredNull = false;
+
+		// Create Queue
 		Queue<BinaryTreeNode<T>> discovered = new LinkedList<BinaryTreeNode<T>>();
+
+		// Add root node
 		discovered.add(node);
 
 		while (!discovered.isEmpty()) {
+
+			// Visit node
 			BinaryTreeNode<T> visited = discovered.remove();
 
 			if (visited.getLeft() != null) {
-				if (flag == true) {
+				// If encountered null node already then return
+				if (encounteredNull == true) {
 					return false;
 				}
+
+				// Keep adding it to queue for further traversal
 				discovered.add(visited.getLeft());
 			} else {
-				flag = true;
+				// If left is null then mark flag to true. It will be used in subsequent calls
+				encounteredNull = true;
 			}
 
 			if (visited.getRight() != null) {
-				if (flag == true) {
+				// If encountered null node already then return
+				if (encounteredNull == true) {
 					return false;
 				}
+
+				// Keep adding it to queue for further traversal
 				discovered.add(visited.getRight());
 			} else {
-				flag = true;
+				// If right is null then mark flag to true. It will be used in subsequent calls
+				encounteredNull = true;
 			}
 		}
 
-		return flag;
+		return encounteredNull;
 	}
 
 }
